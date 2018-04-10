36f754a6a65378c1bc8cfede2bea8dd4c10f892f
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-10 21:42:31 +0000

    I learned how to print last digit trial

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 remainder = number % 10
 if number > 5:
-    print("Last digit of {} is {} and is greater than 5".
-          format("number", "number"))
+    print("Last digit of {} is {} and is greater than 5".format
+          (number, remainder))
 if number == 0:
     print("Last digit of {} is {} and is 0".format(number))
 if number < 6 and number != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".
-          format("number", "number"))
+    print("Last digit of {} is {} and is less than 6 and not 0".format
+          (number, remainder))

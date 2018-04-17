37ec37b3165a223c239b30443f45e3935ebca980
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-17 22:15:00 +0000

    I learned how to print a list of integers (:

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-for n in my_list:
-    print("{}".format(n))
+def print_list_integer(my_list=[]):
+    my_list = [1, 2, 3, 4, 5]
+    for n in my_list:
+            print("{}".format(n))

1197b31739f6eb45b7e599834dc39dfd261287ea
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-18 04:29:55 +0000

    2nd attempt added empty check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     holder = my_list[0]
+    if my_list == "":
+        return None
     for n in my_list:
         if n > holder:
             holder = n

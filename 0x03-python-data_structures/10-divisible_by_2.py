737049f51e947bd8d6c4770d4e57c4a07a34fdae
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-18 05:04:24 +0000

    1st attempt find all the divisibles of 2 using %

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    trueorfalse = []
+    for n in my_list[]:
+        if n % 2 == 0:
+            trueorfalse.append(True)
+        if n % 2 != 0:
+            trueorfalse.append(False)
+            return (trueorfalse)

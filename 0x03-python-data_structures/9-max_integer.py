edb25d0330941bc55675598348931b4bfc43930e
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-18 04:45:58 +0000

    new push with new fail check placement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    holder = my_list[0]
     if len(my_list) == 0:
         return None
-    holder = my_list[0]
     for n in my_list:
         if n > holder:
             holder = n

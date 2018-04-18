669c276d6ca955c4f72b98e951185f3aad62f0b0
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-18 04:39:06 +0000

    new commit for adding fail check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    holder = my_list[0]
-    if my_list == "":
+    if my_list == [[]]:
         return None
+    holder = my_list[0]
     for n in my_list:
         if n > holder:
             holder = n
-    return holder
+            return holder

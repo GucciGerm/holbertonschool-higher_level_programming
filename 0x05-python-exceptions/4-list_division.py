9a6eac0280cd2ecda1994338659ecb06bcacb885
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-30 23:53:20 +0000

    I finally learned how to divide two lists by using try and except

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    newlist = [0] * list_length
+    for n in range(list_length):
+        try:
+            newlist[n] = my_list_1[n] / my_list_2[n]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+    return newlist

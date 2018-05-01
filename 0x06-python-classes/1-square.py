dfbbb0f0a0a8171fc9b7211839858b5e56893c9d
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-01 23:40:42 +0000

    I learned how to add specific attributes to my class i created named square with googledoc

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        self.__size = size
+""" created the square class
+    using the init method we gave it a private istance attributes like size """

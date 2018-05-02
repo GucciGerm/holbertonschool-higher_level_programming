85831034a66da93a9f990c1ac1eebb638d180a69
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-02 03:21:19 +0000

    I learned how to print a square including size attributes with properties and size.setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+class Square:
+    """ intializing square class """
+    def __init__(self, size=0):
+        """
+        init allows us to create an instance
+        and gave it attributes like size(set to 0). We also accounted for
+        exceptions like if it was an integer or if the size was less than zero.
+
+        Args:
+        self - allows an object to refer to itself
+        size - is the size of the square
+
+        Return:
+        None
+
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        size - Size is the size of the of the square.
+
+        Args:
+        self - allows an object to refer to itself
+
+        Return:
+        Going to return the size of the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        size - Size is the size of the square.
+
+        Args:
+        self - allows an object to refer to itself
+        value - Value is the size we are going to set
+
+        Return:
+        None, going to set the size to the current value
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ initializing func to return square area by multiplying """
+        return self.__size * self.__size

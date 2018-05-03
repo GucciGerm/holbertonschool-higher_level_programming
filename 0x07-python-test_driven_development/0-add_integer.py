90ecfe49ec2155fa87d5dd90690c5aaa0709d14c
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-03 01:34:35 +0000

    I learned how to add two integers with documentation

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+y#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    add_integer - We will be adding two integers
+
+    Args:
+    a - first variable
+    b - second variable
+       - b is set to 98
+
+    Return:
+    the addition of a & b
+
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

7d2e5f755186c472f872c7a0c7973d000da634a6
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-07 23:52:43 +0000

    Adding all current contents

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):    # here we are defining our addition function (:
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):    # here we are defining our subtraction fun
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):    # here we are defining our multiplication fun
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):    # here we are defining our division fun
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
---
+++ 0x06-python-classes/4-squaree.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Square:
+    """ intializing square class """
+    def __init__(self, size=0):
+        """
+        init allows us to create an instance
+        and gave it attributes like size(set to 0). We also accounted for
+        exceptions like if it was an integer or if the size was less than zero.
+
+        Args:
+        self - allows an object to refer to itself
+        size - is the size of the square
+
+        Return:
+        None
+
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            self.__size = value
+
+        def area(self):
+            """ initializing func to return square area by multiplying """
+            return self.__size * self.__size

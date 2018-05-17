3bbe60d965dd1b97931479f45e0dbf72a24f2d02
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-17 03:52:30 +0000

    Created a python list consisting of save and load

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+"""
+Here we will be importing our save_to_json_file and load_from_json_file
+"""
+
+save_to_json_file = __import__("7-save_to_json_file").7-save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").8-load_from_json_file
+
+try:
+    load = load_from_json_file("8-load_from_json_file")
+
+except:
+    load = []
+
+    for n, count in enumerate(argv):
+        if n > 0:
+            load.append(count)
+    save_to_json(text, "add_item.json")

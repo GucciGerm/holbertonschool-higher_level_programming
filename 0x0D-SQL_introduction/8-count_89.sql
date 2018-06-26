0a68f380f2fe0623e4cd346a3788910e5f372b92
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-26 04:25:43 +0000

    Adding important From statement

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- This script will display the number of records with id = 89 in first_table
 -- table of database hbtn_0c_0 in MySql server
-SELECT COUNT(*)first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;

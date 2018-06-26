674f658abf090ad301f65615e13a465c1f3aa0fa
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-26 04:44:43 +0000

    Replaced the Show with select to return a set of records

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- This script lists all records of the table second_table of the hbtn_0c_0
 -- database in MySQL serverd_table
-SHOW score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;

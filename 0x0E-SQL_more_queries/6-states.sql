3cd6a52f924ea9070b37e815da94bf08c153c274
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-27 00:33:50 +0000

    Adding how to create a database and use it to contain a table with a unique integer autogenerated non null and primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- This script will create a database hbtn_0d_usa and a states table
+-- id input should be unique, auto-generated, primary key and not null
+-- If the database hbtn_0d_usa and states table both individually exist
+-- the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_GENERATED PRIMARY KEY NOT
+NULL, name VARCHAR(256) NOT NULL);

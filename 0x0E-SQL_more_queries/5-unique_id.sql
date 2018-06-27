420cf40dd1929a815016700a8cffbae3f21d3f49
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-27 00:18:28 +0000

    Removed extra 'int' contained in argument statement, meant to put id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- This script will create a table unique_id on MySQL server
 -- will have an id integer with a default value of 1 also unique
-CREATE TABLE IF NOT EXISTS unique_id (int INT DEFAULT 1 UNIQUE,
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,
 name VARCHAR(256));

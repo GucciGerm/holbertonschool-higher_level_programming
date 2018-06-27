0c4be712cdf68ce6b5ec3fae323b278934234091
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-27 00:10:48 +0000

    Adding how to create table with id integer input with default value of 1, made changes no need for = for default 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- This script will create a table id_not_null on MySQL server
 -- id is an integer with default value of 1
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT=1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));

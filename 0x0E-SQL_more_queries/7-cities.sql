c7ed040d719e0a1f566b047ccf6e94eb496f17a0
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-27 00:51:22 +0000

    Adding how to create a database along with a table that has arguments that contain a primary key and a foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- This script will create a database hbtn_0d_usa and create table cities
+-- within the hbtn_0d_usa, id must be a unique integer, auto generated value
+-- and primary key, state_id must be an integer can't be null and is the
+-- foreign key that references the id of the states table, name varchar can't
+-- be empty
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL
+PRIMARY_KEY, state_id INT NOT NULL, FOREGIN KEY(state_id) REFERENCES states(id)
+name VARCHAR(256) NOT NULL);

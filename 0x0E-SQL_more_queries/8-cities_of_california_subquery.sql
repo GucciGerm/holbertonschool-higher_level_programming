ea2ee089c0c338ad9c61b095255dbc7b039a870e
[GucciGerm] <germaine.balmania@gmail.com>
2018-06-27 02:56:33 +0000

    updated listing all the cities of Caliridonia in the database of hbtn_0d_usa by reconfiguring the placement

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- the database hbtn_0d_usa
 -- The states table contains only 1 record where name = California
 -- Results must be in ascending order by cities.id
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states
-WHERE name = California);
+SELECT id, name FROM cities
+WHERE state_id = (SELECT ID FROM states WHERE name = California);

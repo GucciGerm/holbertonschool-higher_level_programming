94f63102f8991a7f255356a8c2724052d81fd9d2
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-31 22:15:06 -0700

    Updated the peak file to account for if only one element within given list just return that element

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,6 +15,9 @@ def find_peak(list_of_integers):
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
+    if len(list_of_integers) == 1:
+        return (list_of_integers[0])
+
     mid = len(list_of_integers) / 2
     mid = int(mid)
 

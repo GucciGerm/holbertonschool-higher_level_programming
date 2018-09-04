b3072129fe55138f9d3808226c50bc0355850bc9
[GucciGerm] <germaine.balmania@gmail.com>
2018-09-04 16:54:00 -0700

    Adding function that will return the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// This script will return the reversed version of contents a list
+
+
+exports.esrever = function (list) {
+  let counter;
+  let holder = [];
+
+  // get end of list for start point then we need to add element as we -1
+  for (counter = list.length -1; counter >= 0; counter--) {
+    holder.push(list[counter]);
+  }
+  return (holder)
+}

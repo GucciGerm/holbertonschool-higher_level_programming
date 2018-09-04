05dfa744a5a1623676f482392059393516b4672a
[GucciGerm] <germaine.balmania@gmail.com>
2018-09-04 16:54:55 -0700

    Adding semistandard edits

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,14 +2,13 @@
 
 // This script will return the reversed version of contents a list
 
-
 exports.esrever = function (list) {
   let counter;
   let holder = [];
 
   // get end of list for start point then we need to add element as we -1
-  for (counter = list.length -1; counter >= 0; counter--) {
+  for (counter = list.length - 1; counter >= 0; counter--) {
     holder.push(list[counter]);
   }
-  return (holder)
-}
+  return (holder);
+};

e6d22a8f2941991d0589b322052ef3bb7b5a6a82
[GucciGerm] <germaine.balmania@gmail.com>
2018-09-04 11:20:05 -0700

    Adding Javascript script tht defines a rectangle with constructors w and h

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// This script will define a rectangle class with constructors h and w
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

f12fa273c761cda627e332d881c82caa7a4a8d54
[GucciGerm] <germaine.balmania@gmail.com>
2018-09-05 16:38:17 -0700

    Adding script to compute the num of tasks completed by users

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+// This script computes the number of tasks completed by user id
+
+let request = require('request');
+let completed = {};
+
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    let arg = JSON.parse(body);
+    let count;
+    for (count = 0; count < arg.length; count++) {
+      let objective = arg[count];
+      if (objective['completed'] === true) {
+        if (completed[objective['userId']] !== undefined) {
+          completed[objective['userId']] += 1;
+        } else {
+          completed[objective['userId']] = 1;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
